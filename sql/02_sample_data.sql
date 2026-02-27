7d87c2c76c58aa1e0723ad6abe6f7d153b612fda
Sabri Hamdaoui <sabrihamdaoui7@gmail.com>
2026-02-26 23:29:03 -0500

    Add sample data SQL queries

---
+++ sql/02_sample_data.sql
@@ -0,0 +1,9 @@
+-- =====================================================
+-- Sample Data Queries
+-- Description: Quick previews of each table
+-- =====================================================
+
+SELECT * FROM customers LIMIT 10;
+SELECT * FROM products  LIMIT 10;
+SELECT * FROM orders    LIMIT 10;
+SELECT * FROM order_items LIMIT 10;
